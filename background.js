23deda0af814c58e70ba31a3a3264f7abf421a97
Pavel Smelov <pavsmel@mail.ru>
2021-03-07 04:06:36 +0300

    setup for multiply games autowatching

--- background.js
+++ background.js
@@ -1,13 +1,50 @@
 var urlRegex = /^https:\/\/lichess.org\/*/;
+var firstPlayerPoints = 0;
+var secondPlayerPoints = 0;
+var currentTab;
+var flag = true;
 
-function doStuffWithDOM(element) {
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+function addPoints(element){
+    points = element.split("-")
+    if(isNaN(Number(points[0]))){
+        firstPlayerPoints += 0.5;
+        secondPlayerPoints += 0.5;
+    }
+    else{
+        firstPlayerPoints += Number(points[0]);
+        secondPlayerPoints += Number(points[1]); 
+    }
+    console.log(firstPlayerPoints, secondPlayerPoints);
+}
+
+function gameResultRequest(element) {
     console.log(element);
+    if(element == "active"){
+        flag = false;
+        chrome.tabs.sendMessage(currentTab.tabId, { text: "gameResult" }, addPoints);
+        chrome.tabs.sendMessage(currentTab.tabId, { text: "switchGame" });
+    }
 }
 
 chrome.webNavigation.onCompleted.addListener(function(tab) {
-    console.log(tab)
+    currentTab = tab;
     if (urlRegex.test(tab.url)) {
-        chrome.tabs.sendMessage(tab.tabId, { text: "click" },
-                                doStuffWithDOM);
+        chrome.tabs.sendMessage(currentTab.tabId, { text: "click" });
     }
+    if(false){
+        chrome.tabs.sendMessage(currentTab.tabId, { text: "watch" });
+
+        gameEndChecker = async function(){
+            while(flag){
+                chrome.tabs.sendMessage(currentTab.tabId, { text: "checkGameEnd" }, gameResultRequest)
+                await sleep(1000);
+            }
+        }
+        gameEndChecker();
+    }
+        
 });
--- content.js
+++ content.js
@@ -1,6 +1,42 @@
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.text && (msg.text == "click")) {
-        setTimeout(() => { console.log(document.getElementsByClassName('friend_box_title')[0].click()) }, 1000);
-        sendResponse(document.getElementsByClassName('friend_box_title')[0].innerHTML);
+        setTimeout(() => { document.getElementsByClassName("friend_box_title")[0].click() }, 1000);
+    }
+    else if (msg.text && (msg.text == "watch")){
+        setTimeout(() => {
+            buttons = document.getElementsByClassName("fbt")
+            for(var i = 0; i<buttons.length; i++){
+                if(buttons[i].dataset.act == "first"){
+                    var clickEvent = document.createEvent("MouseEvents");
+                    clickEvent.initEvent("mousedown", true, true);
+                    buttons[i].dispatchEvent(clickEvent);
+                }
+                if(buttons[i].title == "Menu"){
+                    var clickEvent = document.createEvent("MouseEvents");
+                    clickEvent.initEvent("mousedown", true, true);
+                    buttons[i].dispatchEvent(clickEvent);
+                }
+            }
+
+            buttons = document.getElementsByClassName("button button-empty");
+            for (var i = 0; i < buttons.length; i++){
+                if(buttons[i].textContent == "Realtime"){
+                    buttons[i].click();
+                };
+            }
+        }, 0);
+    }
+    else if (msg.text && (msg.text == "checkGameEnd")){
+        moves = document.getElementsByTagName("move");
+        if(moves.length == 0){
+            sendResponse("Error");
+        }
+        sendResponse(moves[moves.length-1].className);
+    }
+    else if (msg.text && (msg.text == "gameResult")){
+        sendResponse(document.getElementsByClassName("result")[0].innerText);
+    }
+    else if (msg.text && (msg.text == "switchGame")){
+
     }
 });
